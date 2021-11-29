3f13c8c7c36d8f5d98d4ee78c9906db1103df6ea
ziruochou <ziruo@live.unc.edu>
2021-11-29 16:13:16 -0500

    0

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -9,7 +9,7 @@
  * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  */
 export function sumToString(a, b) {
-
+return "a + b = "+ (a+b);
 }
 
 
@@ -24,7 +24,11 @@ export function sumToString(a, b) {
  *
  */
 export function getIncreasingArray(startNumber, endNumber) {
-
+    var array = new Array[endNumber-startNumber+1]
+    for (i=0; i<endNumber-startNumber+1; i++){
+    array[i]=startNumber+i;
+    }
+return array;
 }
 
 /**
