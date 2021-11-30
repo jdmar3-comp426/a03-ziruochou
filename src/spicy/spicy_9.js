63c9f54db3c73ff80789ba0127748dc23f8199f8
ziruochou <ziruo@live.unc.edu>
2021-11-30 14:44:59 -0500

    all evens are odd

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -163,7 +163,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+   return everyEven(arr,x => x%2 === 1);
 };
 
 
